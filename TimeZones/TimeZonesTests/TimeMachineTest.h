63370bad54627b39cb4a3ce68bd73fa6413d0948
sebastian cruz <jsebas.ct@gmail.com>
2013-10-02 07:44:55 -0400

    Removing some unnecessary folders

---
+++ TimeZones/TimeZonesTests/TimeMachineTest.h
@@ -0,0 +1,13 @@
+//
+//  TimeMachineTest.h
+//  TimeZones
+//
+//  Created by Sebastian Cruz on 10/2/13.
+//  Copyright (c) 2013 PeopleSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeMachineTest : NSObject
+
+@end
---
+++ TimeZones/TimeZonesTests/TimeMachineTest.m
@@ -0,0 +1,13 @@
+//
+//  TimeMachineTest.m
+//  TimeZones
+//
+//  Created by Sebastian Cruz on 10/2/13.
+//  Copyright (c) 2013 PeopleSoft. All rights reserved.
+//
+
+#import "TimeMachineTest.h"
+
+@implementation TimeMachineTest
+
+@end
